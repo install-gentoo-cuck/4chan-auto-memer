a5274c2ddc390c52bb847c9b6712ca1c9fe684f0
Phillip <felipe@chichasov.es>
2015-06-10 22:52:11 +0200

    Added automatic autism

--- 4chan.py
+++ 4chan.py
@@ -1,22 +1,74 @@
+from urllib2 import urlopen
+from urllib import urlencode
 from random import choice, randint
-chanVerbs = ['tips fedora ', 'kill yourself ', 'desu ', 'jew ', 'nigger-rigged ', 'cucked ', 'tfw ', 'ITT: ', 'memed ', 'fizz buzzed ']
-chanObjects = ['fedora ', 'jew ', 'nigger ', 'cuck ', 'reddit ', 'tumblr ', 'bane ', 'ITT: ', 'faggot ','meme ', 'autist ', 'waifu ', 'semen demon ', 'fizz buzz ', 'trap ',': the thread ', 'doge ', 'pleb ', 'feel ']
-chanObjectModifiers = ['a shit', 'fedora tipping ', 'an-heroing ', 'JUST ', 'jew ', 'nigger ', 'cuck ', 'based ', 'reddit-tier ', 'tumblr-tier ', 'baneposting ', 'literally ', 'faggot ','meme ', 'autistic ', 'fizz buzz-tier ', 'shit tier ', 'god tier ',':the thread ', 'pleb ', 'for free ','objectively ']
-chanGarbage = ['Argentina is white', 'jet fuel can\'t melt steel beams','install gentoo', 'ayy lmao', 'it\'s happening', 'REEEEEEEEEEEEEEEEEEEEEEEEEEE', 'huehuehuehuehue', 'umad?', '''>>>/pol/''']
-	
-out = ''
-i=0; j=0; k=0
-while i < randint(3,10):
-	out=choice(chanObjects)
-	while j < randint(2,5):
-		out+=choice(chanObjectModifiers)
-		j+=1
-	out+=choice(chanVerbs) 
-	out+= choice(chanObjects)
-	while k < randint(0,2):
-		out+=choice(chanObjectModifiers)
-		k+=1
-	print out, '.',
-	i+=1
-print choice(chanGarbage), '.',
-print '\n/thread'
+from string import ascii_lowercase, ascii_uppercase
+from time import sleep
+import SimpleHTTPServer, SocketServer, thread
+
+with open("captchapage.html", "w") as capHTML:
+	fetch = urlopen("http://a.uguu.se/yjlhye_CaptchaPage.html")
+	capHTML.write(fetch.read())
+
+class CaptchaServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/captchapage.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+    def log_message(self, format, *args):
+    	pass
+
+def createPost(board, threadid, message, captcha, name='Anonymous'):
+	url = 'https://sys.4chan.org/%s/post'  % board
+	alphanumeric = ascii_lowercase
+	alphanumeric += ascii_uppercase
+	alphanumeric += '0123456789'
+	pwd = ''
+	for n in range(32):
+		pwd += choice(alphanumeric)
+	form = {
+		'MAX_FILE_SIZE':'4194304',
+		'pwd': pwd,
+		'mode':'regist',
+		'resto':threadid,
+		'email':'',
+		'upfile':'',
+		'name':name,
+		'com':message,
+		'g-recaptcha-response': str(captcha)
+	}
+	data = urlencode(form)
+	result = urlopen(url, data=data)
+	return result.getcode()
+
+def hostCaptcha():
+	handler = CaptchaServer
+	serv = SocketServer.TCPServer(("", 8888), handler)
+	print 'Visit localhost:8888 for the captcha.'
+	serv.serve_forever()
+thread.start_new_thread(hostCaptcha, ())
+
+sleep(1)
+while True:
+	cap = raw_input('Enter a captcha: ')
+	cv = ['tips fedora ', 'kill yourself ', 'desu ', 'jew ', 'nigger-rigged ', 'cucked ', 'tfw ', 'ITT: ', 'memed ', 'fizz buzzed ']
+	co = ['fedora ', 'jew ', 'nigger ', 'cuck ', 'reddit ', 'tumblr ', 'bane ', 'ITT: ', 'faggot ','meme ', 'autist ', 'waifu ', 'semen demon ', 'fizz buzz ', 'trap ',': the thread ', 'doge ', 'pleb ', 'feel ']
+	com = ['a shit', 'fedora tipping ', 'an-heroing ', 'JUST ', 'jew ', 'nigger ', 'cuck ', 'based ', 'reddit-tier ', 'tumblr-tier ', 'baneposting ', 'literally ', 'faggot ','meme ', 'autistic ', 'fizz buzz-tier ', 'shit tier ', 'god tier ',':the thread ', 'pleb ', 'for free ','objectively ']
+	cg = ['Argentina is white', 'jet fuel can\'t melt steel beams','install gentoo', 'ayy lmao', 'it\'s happening', 'REEEEEEEEEEEEEEEEEEEEEEEEEEE', 'huehuehuehuehue', 'umad?', '''>>>/pol/''']
+	go = ''
+	out = ''
+	i=0; j=0; k=0
+	while i < randint(3,10):
+		out=choice(co)
+		while j < randint(2,5):
+			out+=choice(com)
+			j+=1
+		out+=choice(cv) 
+		out+= choice(co)
+		while k < randint(0,2):
+			out+=choice(com)
+			k+=1
+		go += out + '\n'
+		i+=1
+	go += choice(cg) + '.'
+	go += '\n\n/thread'
+	print "R: " + str(createPost('g', '48431348', go, cap))
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 4chan-auto-memer
-AA - Artificial Autism to make you sound just like a 4channer
+AA - Artificial Autism to make you sound just like an autist
+
+#Usage
+Start the auto memer and select a thread to meme. Next navigate to "localhost:8888", next solve the captcha and copy the text that just appeared to the console window and hit enter. Rinse and repeat.
